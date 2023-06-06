5491d49abc200cc8efd2886f58a664a3a9ef14bd
joseph cruz galaviz <josephcruzgalaviz20@gmail.com>
2023-06-05 18:28:51 -0600

    http response

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
@@ -10,12 +12,8 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  res.json({
-    name: "Product 1",
-    price: 1000
-  });
-});
+routerApi(app);
+
 
 app.listen(port, () => {
   console.log('Mi port: ' + port);
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const productsRouter = require('./products.router');
+const userRouter = require('./users.router');
+const categoriesRouter = require('./categories.router');
+
+function routerApi(app) {
+
+  app.use('/products', productsRouter);
+  app.use('/users', userRouter);
+  app.use('/categories', categoriesRouter);
+
+}
+
+module.exports = routerApi;
